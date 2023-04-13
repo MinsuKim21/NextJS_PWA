e3f3cddacacac893e58788b6e99cba965e1ea308
MinsuKim <minsu020302@gmail.com>
2023-04-13 10:23:05 +0900

    project init

--- next.config.js
+++ next.config.js
@@ -1,8 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-}
 
-module.exports = nextConfig
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+})
+
+const withOffline = require('next-offline')
+
+module.exports = withPWA({
+  // next.js config
+  experimental: {
+    // appDir: true,
+  },
+  withOffline,
+})
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,8 +9,20 @@ module.exports = {
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      spacing: {
+        18: '4.5rem',
+        30: '7.5rem',
+        88: '22rem',
+        104: '26rem',
+        112: '28rem',
+        128: '32rem',
+        mobileScreen: 'calc(var(--vh, 1vh) * 100)',
+      },
+      colors: {
+        myGreen: '#00Fd00',
+        myPupple: '#BE19FC',
       },
     },
   },
