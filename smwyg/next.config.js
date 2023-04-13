9663e154811b548c4cdc090ead70fa72bf4abcb6
MinsuKim <minsu020302@gmail.com>
2023-04-13 10:59:23 +0900

    Project init

---
+++ smwyg/next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa');
+
+const config = {
+  // ...원래 next config 파일
+  reactStrictMode: true,
+};
+
+const nextConfig = withPWA({
+  dest: 'public',
+  runtimeCaching: [],
+})(config);
+
+module.exports = nextConfig;
