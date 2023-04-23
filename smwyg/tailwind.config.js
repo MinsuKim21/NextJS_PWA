f18ea24b36fae800f7dfb2c5f1477dc883d2b29c
MinsuKim <minsu020302@gmail.com>
2023-04-23 23:41:27 +0900

    Chore : tailwind config

--- smwyg/tailwind.config.js
+++ smwyg/tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}', './src/app/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/app/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
     extend: {
       backgroundImage: {
@@ -20,7 +24,22 @@ module.exports = {
         myGreen: '#00Fd00',
         myPupple: '#BE19FC',
       },
+      keyframes: {
+        bounceSlow: {
+          '0%, 100%': {
+            transform: 'translateY(-30%)',
+            animationTimingFunction: 'cubic-bezier(0.5,0,0,0)',
+          },
+          '50%': {
+            transform: 'none',
+            animationTimingFunction: 'cubic-bezier(0.5,0,0,0)',
+          },
+        },
+      },
+      animation: {
+        'bounce-slow': 'bounceSlow 1.5s infinite',
+      },
     },
   },
   plugins: [],
-};
+}
