f2f2aa4b9a37e113d1438adeba7c6bf89169ca91
MinsuKim <minsu020302@gmail.com>
2023-04-24 13:04:39 +0900

    Feat : skeleton추가

--- smwyg/src/pages/blog/index.tsx
+++ smwyg/src/pages/blog/index.tsx
@@ -1,13 +1,27 @@
+import Skeleton from '@/components/Skeleton'
+import { useState } from 'react'
+
 export default function Blog() {
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+
   return (
-    <main className="pt-20 flex flex-col space-y-10 justify-around px-20 h-full">
+    <main className="pt-20 flex flex-col space-y-10 justify-around sm:px-20 h-full">
       <iframe
         src="https://velog.io/tags"
         height="600"
+        width="100%"
         sandbox=""
         seamless
-        className="overflow-y-hidden"
+        onLoad={() => setIsLoading(true)}
+        className="overflow-y-hidden fixed z-10 top-14"
       />
+      {isLoading ? null : (
+        <div className="mt-2 mx-4 space-y-4">
+          <Skeleton />
+          <Skeleton />
+          <Skeleton />
+        </div>
+      )}
     </main>
   )
 }
