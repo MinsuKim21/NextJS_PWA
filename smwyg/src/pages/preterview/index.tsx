823a60261f5537f9cf648757e8c7699f0fd60c80
MinsuKim <minsu020302@gmail.com>
2023-04-24 09:14:31 +0900

    Chore : build test를 위해 올림

--- smwyg/src/pages/preterview/index.tsx
+++ smwyg/src/pages/preterview/index.tsx
@@ -1,7 +1,7 @@
 export default function Preterview() {
   return (
-    <main className="mx-40">
-      <h1>index</h1>
+    <main className="pt-20 flex flex-col space-y-10 justify-around px-20">
+      <div className="w-full text-center text-2xl font-black text-primary">공사중</div>
     </main>
   )
 }
