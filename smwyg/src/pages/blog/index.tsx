4da566bdd2394a7deadf0a7ca948c5e855a67b16
MinsuKim <minsu020302@gmail.com>
2023-04-24 09:11:00 +0900

    Rename : 파일 이름 수정

---
+++ smwyg/src/pages/blog/index.tsx
@@ -0,0 +1,7 @@
+export default function Blog() {
+  return (
+    <main className="pt-20 flex flex-col space-y-10 justify-around px-20">
+      <div className="w-full text-center text-2xl font-black text-primary">공사중</div>
+    </main>
+  )
+}
--- smwyg/src/pages/contect/index.tsx
+++
@@ -1,7 +0,0 @@
-export default function Contect() {
-  return (
-    <main className="mx-40">
-      <h1>index</h1>
-    </main>
-  )
-}
