86408c907fdc9c5162feefec109c159117edbca5
MinsuKim <minsu020302@gmail.com>
2023-04-27 18:58:29 +0900

    Feat : 로그인 완성

--- smwyg/src/pages/preterview/index.tsx
+++ smwyg/src/pages/preterview/index.tsx
@@ -1,7 +1,28 @@
+import ContextBox from '@/components/ContextBox'
+import { signIn, signOut, useSession } from 'next-auth/react'
+
 export default function Preterview() {
+  const { data: session } = useSession()
+  console.log(session)
+
   return (
-    <main className="pt-20 flex flex-col space-y-10 justify-around sm:mx-20 mx-10">
-      <div className="w-full text-center text-2xl font-black text-primary">공사중</div>
+    <main className="pt-20 flex flex-col space-y-5 justify-around md:mx-20 ">
+      {session ? (
+        <button
+          className="btn btn-ghost bg-yellow-400 text-black hover:text-yellow-200"
+          onClick={() => signOut()}
+        >
+          로그아웃
+        </button>
+      ) : (
+        <button
+          className="btn btn-ghost bg-yellow-400 text-black hover:text-yellow-200"
+          onClick={() => signIn('kakao')}
+        >
+          카카오 로그인
+        </button>
+      )}
+      {session ? <ContextBox secContext={session?.user?.name + '님 반갑습니다.' + ''} /> : <></>}
     </main>
   )
 }
