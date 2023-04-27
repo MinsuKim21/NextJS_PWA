3a99f2061074dcf1c52999a96faf92f68567d5f4
MinsuKim <minsu020302@gmail.com>
2023-04-27 16:39:04 +0900

    Fix : font size 확장으로 변경

--- smwyg/tailwind.config.js
+++ smwyg/tailwind.config.js
@@ -6,11 +6,11 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    fontSize: {
-      '2xs': '0.6rem',
-      '3xs': '0.375rem',
-    },
     extend: {
+      fontSize: {
+        '2xs': '0.6rem',
+        '3xs': '0.375rem',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
