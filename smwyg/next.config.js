1b3d08f419fa4348e563a6e5722886852d13a076
MinsuKim <minsu020302@gmail.com>
2023-04-27 16:47:58 +0900

    Chore  : Next image config

--- smwyg/next.config.js
+++ smwyg/next.config.js
@@ -4,7 +4,10 @@ const withPWA = require('next-pwa');
 const config = {
   // ...원래 next config 파일
   reactStrictMode: true,
-};
+  images: {
+    domains: ['k.kakaocdn.net'],
+  },
+}
 
 const nextConfig = withPWA({
   dest: 'public',
